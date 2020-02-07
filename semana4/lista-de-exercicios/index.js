f929221c102bf51d3c2471eb0c25697a836ec389
eloisa.fagundes <lola.fernanda@hotmail.com>
2020-02-07 11:56:28 -0300

    exercício 1 resolvido (menos o 4)

---
+++ semana4/lista-de-exercicios/index.js
@@ -0,0 +1,18 @@
+/*  INTERPRETAÇÃO DE CÓDIGO
+1. O código é um conversor de dólares em real. O usuário tem 100 dólares e vai converter esse valor 
+em reais. O console retorna 430.
+
+2. O código faz uma lista de opções de investimento e devolve prousuário o valor após o investimento.
+Caso o usuário não digite nenhum valor, um alert é impresso solicitando um valor para executar o 
+cálculo. O console imprime novoMontante como R$165 e segundoMontante exibe o alet "TIPO DE 
+INVESTIMENTO INCORRETO!" pois não foi declarado um case de Tesouro Direto.
+
+3. Esse código faz um loop utilizando o for of para trazer duas novas variações com o array.push. 
+Se numero no array1 tiver resto da divisão igual a zero o console deve imprimir sua quantidadade
+através do array1.lenght, senão se não tiver resto da divisão igual a zero imprime no console através 
+do array2.lenght. As impressões no console serão Quantidade total de numeros 14, 6 e 8 
+(resultados respectivamente dequantidade de elementos de numeros.lenght, array1.lenght e array2.lenght).
+
+4.
+*/
+
