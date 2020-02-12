c653fcad8360259aa0dde0de01cb8fb88b88c1ff
eloisa.fagundes <lola.fernanda@hotmail.com>
2020-02-12 14:14:04 -0300

    arquivos que não subiram ontem

---
+++ semana5/componentes-funcionais/portifa/build/precache-manifest.cd5c7f72b6b29f4673ffbd059796e1bf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "782d28dda1f6265b48a19b7d6c7f4b3d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b8fbee53cd430bacd56e",
+    "url": "/static/css/main.cf3ec2d3.chunk.css"
+  },
+  {
+    "revision": "a8a6b13fb5505a7d91f6",
+    "url": "/static/js/2.1d328be4.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.1d328be4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b8fbee53cd430bacd56e",
+    "url": "/static/js/main.a7aa7afc.chunk.js"
+  },
+  {
+    "revision": "ceceb449e0fd409e97c8",
+    "url": "/static/js/runtime-main.6ffd53ad.js"
+  }
+]);
