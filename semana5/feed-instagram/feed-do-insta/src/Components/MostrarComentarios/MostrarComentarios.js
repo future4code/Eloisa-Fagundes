e14a6e3de6d0bd81ba94e330862b0f0e4e2a69c6
eloisa.fagundes <lola.fernanda@hotmail.com>
2020-02-16 01:07:37 -0300

    adiciona esconder comentário

--- semana5/feed-instagram/feed-do-insta/src/Components/MostrarComentarios/MostrarComentarios.js
+++ semana5/feed-instagram/feed-do-insta/src/Components/MostrarComentarios/MostrarComentarios.js
@@ -18,28 +18,36 @@ class MostrarComentarios extends React.Component {
     super(props)
     this.state = {
       count: 0,
+      showComment: false
     }
   }
 
   comment = () => {
     this.setState({
-      count: this.state.count +1
+      count: this.state.count + 1
     })
-
+    this.showComment()
   }
 
+  showComment = () => {
+    this.setState({
+      showComment: !this.state.showComment
+    })
+  }
 
   render() {
+    console.log(this.state)
     return (
       <div>
         <IconAndCount>
-        <img src={iconeComentarioFeed} alt="comentario" />
-        <div>{this.state.count}</div>
+          <img onClick={this.showComment} src={iconeComentarioFeed} alt="comentario" />
+          <div>{this.state.count}</div>
         </IconAndCount>
-        <CommentContainer>
-          <input placeholder="Escreva seu comentário"></input>
-          <button onClick={this.comment}>Comentar</button>
-        </CommentContainer>
+        {this.state.showComment &&
+          (<CommentContainer>
+            <input placeholder="Escreva seu comentário"></input>
+            <button onClick={this.comment}>Comentar</button>
+          </CommentContainer>)}
       </div>
     )
   }
