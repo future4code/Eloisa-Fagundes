4035f9fd7abb2a0d0956252d0c578cff51c34347
eloisa.fagundes <lola.fernanda@hotmail.com>
2020-03-27 17:31:51 -0300

    remove reducer planets da list de reducers

--- semana10/projeto-da-semana/src/reducers/index.js
+++ semana10/projeto-da-semana/src/reducers/index.js
@@ -1,12 +1,11 @@
 import { combineReducers } from "redux";
 import { connectRouter } from "connected-react-router";
 import trips from './trips';
-import planets from './planets'
 
 export const generateReducers = history =>
   combineReducers({
     router: connectRouter(history),
     // Outros reducers aqui
     trips,
-    planets,
+
   });
