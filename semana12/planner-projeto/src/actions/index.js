dd7eab2aea3819739f960c01d00481d159e3e11f
eloisa.fagundes <lola.fernanda@hotmail.com>
2020-04-10 17:50:13 -0300

    inicío da implementação com API

--- semana12/planner-projeto/src/actions/index.js
+++ semana12/planner-projeto/src/actions/index.js
@@ -1,13 +1,29 @@
-// adiciona tarefa
+import axios from 'axios'
 
+// adiciona tarefa
  export const addTask = (text) => {
     return {
         type: 'ADD_TASK',
         payload: {
-            // id,
             text,
-            // day
         }
     }
 }
 
+const setTasks = (tasks) => ({
+    type: "SET_TASKS",
+    payload: {
+        tasks,
+    }
+})
+
+const createTask = (registerTask) => async (dispatch) => {
+  console.log("Chegou", registerTask)
+    
+    try {
+
+    } catch (error) {
+        console.error(error.message)
+        alert("Não foi possível criar sua tarefa!")
+    }
+}
