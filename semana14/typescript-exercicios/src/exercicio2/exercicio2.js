e38d388eddd76cbeb30d49eb52474012468f5158
eloisa.fagundes <lola.fernanda@hotmail.com>
2020-04-20 20:50:09 -0300

    adiciona exercício obrigatório 2

--- semana14/typescript-exercicios/src/exercicio2/exercicio2.js
+++ semana14/typescript-exercicios/src/exercicio2/exercicio2.js
@@ -1,15 +1,12 @@
 // Faça uma função que receba um array de números e retorne um objeto que tenha as seguintes informações: 
-// 1) a quantidade de números que há no array
-// 2) a quantidade de números ímpares no array
-// 3) soma de todos os elementos do array
 var arr = [1, 2, 3, 4, 5, 6];
+// 1) a quantidade de números que há no array
 var quantidadeDeNumeros = (arr);
 console.log(quantidadeDeNumeros.length); //6
-var numerosImpares = function (arr) {
-    if (arr.number % 2 == 0) {
-        return true;
-    }
-    else {
-        return (arr.push);
-    }
-};
+// 2) a quantidade de números ímpares no array
+var quantImpares = arr.filter(function (i) { return i % 2 !== 0; }); //[ 1, 3, 5 ]
+console.log(quantImpares);
+// 3) soma de todos os elementos do array
+var somaArray = function (acc, val) { return acc + val; };
+console.log(arr.reduce(somaArray)); //21
+// Eu fiz assim mas não entendi porque funcionou, mesmo os parâmetros parecendo incorretos.
--- semana14/typescript-exercicios/src/exercicio2/exercicio2.ts
+++ semana14/typescript-exercicios/src/exercicio2/exercicio2.ts
@@ -1,9 +1,15 @@
 // Faça uma função que receba um array de números e retorne um objeto que tenha as seguintes informações: 
-// 1) a quantidade de números que há no array
-// 2) a quantidade de números ímpares no array
-// 3) soma de todos os elementos do array
-
 let arr: number[] = [1, 2, 3, 4, 5, 6]
 
+// 1) a quantidade de números que há no array
 let quantidadeDeNumeros = (arr)
 console.log(quantidadeDeNumeros.length) //6
+
+// 2) a quantidade de números ímpares no array
+let quantImpares = arr.filter(i => i % 2 !== 0)//[ 1, 3, 5 ]
+console.log(quantImpares)
+
+// 3) soma de todos os elementos do array
+const somaArray = (acc, val) => acc + val
+console.log(arr.reduce(somaArray)) //21
+// Eu fiz assim mas não entendi porque funcionou, mesmo os parâmetros parecendo incorretos.
