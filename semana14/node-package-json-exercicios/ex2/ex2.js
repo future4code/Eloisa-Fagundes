9c6f6a2b5943f9947a8e449d102114dd95262927
eloisa.fagundes <lola.fernanda@hotmail.com>
2020-04-22 18:32:18 -0300

    adiciona exercício 2 e 3

---
+++ semana14/node-package-json-exercicios/ex2/ex2.js
@@ -0,0 +1,13 @@
+const fs = require('fs')
+
+const listaDeTarefas = process.argv[2]
+const novaTarefa = process.argv[3]
+const novaTarefaFormatada = `
+${novaTarefa}`
+
+try {
+ fs.appendFileSync( listaDeTarefas, novaTarefaFormatada, 'utf8')
+ console.log("Tarefa criada com sucesso!")
+} catch (err) {
+    console.error(err)
+}
