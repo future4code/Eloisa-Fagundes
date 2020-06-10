a7dd00b1a0bcc2eb1f4f78b896c00dd13254f6a6
eloisa.fagundes <lola.fernanda@hotmail.com>
2020-06-09 23:32:49 -0300

    add exercício 3

--- semana21/aws-lambda/src/exercicio2.ts
+++ semana21/aws-lambda/src/exercicio2.ts
@@ -1,3 +1,12 @@
+export const handler = async (event: any) => {
+    const response = {
+        statusCode: 200,
+        body: JSON.stringify("Olá mundo, sou uma mensagem da AWS")
+    }
+
+    return response
+}
+
 // exports.handler = async (event) => {
 //     const response = {
 //         statusCode: 200,
